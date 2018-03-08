5933a52f9e8a1cd1134379aa93c9613e37300e3d
Maksym Yadvinskyy <maksym.yadvinskyy@reventstudio.com>
2018-03-07 22:14:49 -0600

    Adds communication between react and electron

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import ElectronSupportBanner from './Components/ElectronSupportBanner/ElectronSupportBanner';
+import Mail from './Components/Mail/Mail';
 
 class App extends Component {
   componentDidMount = () => {
@@ -12,6 +13,7 @@ class App extends Component {
       <div className="App">
         <ElectronSupportBanner />
         Some Awesome app
+        <Mail />
       </div>
     );
   }
